b350e1e1b04f77c783e9c06e74d4ebeef07120c3
Wilman <wilmanuls93@gmail.com>
2021-08-12 18:11:14 -0500

    first commit

---
+++ src/components/Pregunta.js
@@ -0,0 +1,56 @@
+import React, {Fragment, useState} from 'react';
+import Error from "./Error"
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
+
+    //definir el State
+
+    const[cantidad, guardarCantidad]=useState(0);
+    const[error, guardarError]=useState(false);
+
+    const definirPresupuesto=e=>{
+        guardarCantidad(parseInt(e.target.value,10))
+
+
+
+    }
+
+    //submit para le presupuesto
+const agregarPresupuesto =e=>{
+    e.preventDefault();
+
+    if(cantidad<1 || isNaN(cantidad) ){
+        guardarError(true);
+        return;
+    }
+    guardarError(false);
+    guardarPresupuesto(cantidad);
+    guardarRestante(cantidad);
+    actualizarPregunta(false);
+
+
+
+}
+
+    return ( 
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+
+            {error? <Error mensaje="El Presupuesto es incorrecto"/>:null}
+            <form onSubmit={agregarPresupuesto}>
+                <input
+                type="number"
+                className="u-full-width"
+                placeholder="Coloca tu presupuesto"
+                onChange={definirPresupuesto}
+                />
+                <input
+                type="submit"
+                className="button-primary u-full-width"
+                value="Definir Presupuesto"
+                />
+            </form>
+        </Fragment>
+     );
+}
+ 
+export default Pregunta ;
